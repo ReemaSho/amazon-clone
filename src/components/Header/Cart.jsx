916211c71c785c46db8ea4e3214a3aafd952babd
Reema <Reemaalshohof88@gmail.com>
2022-03-06 10:34:12 +0100

    add selector to reducer to track the count of items

--- src/components/Header/Cart.jsx
+++ src/components/Header/Cart.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import ShoppingCartCheckoutTwoToneIcon from "@mui/icons-material/ShoppingCartCheckoutTwoTone";
+import { trackCartItemsCount } from "../../context/reducer";
 import { useStateValue } from "../../context/StateContext";
 const Cart = () => {
-  const [{ ItemsCount }] = useStateValue();
+  const [{ Cart }] = useStateValue();
   return (
     <Link to="/checkout" className="header__navLink">
       <div className="header__optionCart">
         <ShoppingCartCheckoutTwoToneIcon className="header__optionLineTwo" />
         <span className="header__optionLineTwo header__CartCount">
-          {ItemsCount}
+          {trackCartItemsCount(Cart)}
         </span>
       </div>
     </Link>
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,3 +1,6 @@
+export const initialState = {
+    Cart: [],
+};
 export const getCartTotal = (cart) => {
     if (cart.length) {
         const amounts = [];
@@ -10,78 +13,78 @@ export const getCartTotal = (cart) => {
         return 0;
     }
 };
-export const initialState = {
-    Cart: [],
-    ItemsCount: 0,
+export const trackCartItemsCount = (cart) => {
+    if (cart.length) {
+        const quantities = [];
+        cart.forEach((item) => {
+            quantities.push(item.quantity);
+        });
+        const total = quantities.reduce((amount, item) => item + amount, 0);
+        return total;
+    } else {
+        return 0;
+    }
 };
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD_TO_CART":
             let newAddCart = [...state.Cart];
-            let addCount1 = state.ItemsCount;
+
             const addIndex = state.Cart.findIndex(
                 (cartItem) => cartItem.id === action.id
             );
             if (addIndex >= 0) {
                 newAddCart[addIndex].quantity += 1;
-
-                addCount1 += 1;
             } else {
-                addCount1 += 1;
                 return {
                     ...state,
                     Cart: [...state.Cart, action.item],
-                    ItemsCount: addCount1,
                 };
             }
 
-            return {...state, Cart: newAddCart, ItemsCount: addCount1 };
+            return {...state, Cart: newAddCart };
 
         case "REMOVE_FROM_Cart":
             let newCRemoveCart = [...state.Cart];
-            let minCount1 = state.ItemsCount;
+
             const removeIndex = state.Cart.findIndex(
                 (cartItem) => cartItem.id === action.id
             );
             if (removeIndex >= 0) {
-                minCount1 = minCount1 - newCRemoveCart[removeIndex].quantity;
                 newCRemoveCart.splice(removeIndex, 1);
             } else {
                 console.warn(
                     `Can't remove product(id: ${action.id}) as its not in cart`
                 );
             }
-            return {...state, Cart: newCRemoveCart, ItemsCount: minCount1 };
+            return {...state, Cart: newCRemoveCart };
         case "INCREMENT_ITEM_QUANTITY":
             let newIncrementedCart = [...state.Cart];
-            let addCount2 = state.ItemsCount;
+
             const indexToIncrement = state.Cart.findIndex(
                 (cartItem) => cartItem.id === action.id
             );
             if (indexToIncrement >= 0) {
                 newIncrementedCart[indexToIncrement].quantity += 1;
-                addCount2 += 1;
             } else {
                 console.warn(
                     `Can't add more items with (id: ${action.id}) as its not in cart`
                 );
             }
 
-            return {...state, Cart: newIncrementedCart, ItemsCount: addCount2 };
+            return {...state, Cart: newIncrementedCart };
 
         case "DECREMENT_ITEM_QUANTITY":
             let newDecrementedCart = [...state.Cart];
-            let minCount2 = state.ItemsCount;
+
             const indexToDecrement = state.Cart.findIndex(
                 (cartItem) => cartItem.id === action.id
             );
             let { quantity } = newDecrementedCart[indexToDecrement];
             if (indexToDecrement !== -1 && quantity > 1) {
                 newDecrementedCart[indexToDecrement].quantity -= 1;
-                minCount2 -= 1;
             } else if (quantity === 1) {
-                minCount2 -= 1;
                 newDecrementedCart.splice(indexToDecrement, 1);
             } else {
                 console.warn(
@@ -89,7 +92,7 @@ const reducer = (state, action) => {
                 );
             }
 
-            return {...state, Cart: newDecrementedCart, ItemsCount: minCount2 };
+            return {...state, Cart: newDecrementedCart };
         default:
             return state;
     }
