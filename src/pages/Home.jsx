0f4fd42e3b9e694e19095fea61fd2ad191105ed3
Reema <Reemaalshohof88@gmail.com>
2022-03-07 16:54:54 +0100

    arrange sass files location in pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 import CarouselSlider from "../components/CarouselSlider";
 import Cards from "../components/Home/Cards";
 import Footer from "../components/Footer";
-import "./sass/homePage.scss";
+import "./home.scss";
 
 const Home = () => {
   return (
